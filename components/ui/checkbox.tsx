294cd4554459bfb0f2841f786145a7df83b672e5
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-11-17 18:21:19 -0800

    whoops, forgot to track the checkbox

---
+++ components/ui/checkbox.tsx
@@ -0,0 +1,13 @@
+import { ChangeEvent } from "react";
+
+export default function Checkbox({ checked, onChange }: {checked: boolean, onChange: (e: ChangeEvent<HTMLInputElement>) => void}) {
+  return (
+    <input
+      type="checkbox"
+      checked={checked}
+      onChange={onChange}
+      className="border-2 bg-pink-100 border-pink-200 rounded-sm p-2"
+      value={1}
+    />
+  )
+}
