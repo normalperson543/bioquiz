2c913181619c75e076703d07346e840ccd608369
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-12-26 00:31:14 -0800

    figure out how to proxy clerk reqs

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { prisma } from "./lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/auth/login(.*)",
@@ -7,7 +8,45 @@ const isPublicRoute = createRouteMatcher([
   "/",
 ]);
 
+function proxyMiddleware(req: NextRequest) {
+  if (req.nextUrl.pathname.match("__clerk")) {
+    const proxyHeaders = new Headers(req.headers);
+    proxyHeaders.set(
+      "Clerk-Proxy-Url",
+      process.env.NEXT_PUBLIC_CLERK_PROXY_URL || "",
+    );
+    proxyHeaders.set("Clerk-Secret-Key", process.env.CLERK_SECRET_KEY || "");
+    if (req.ip) {
+      proxyHeaders.set("X-Forwarded-For", req.ip);
+    } else {
+      proxyHeaders.set(
+        "X-Forwarded-For",
+        req.headers.get("X-Forwarded-For") || "",
+      );
+    }
+
+    const proxyUrl = new URL(req.url);
+    proxyUrl.host = "frontend-api.clerk.dev";
+    proxyUrl.port = "443";
+    proxyUrl.protocol = "https";
+    proxyUrl.pathname = proxyUrl.pathname.replace("/__clerk", "");
+
+    return NextResponse.rewrite(proxyUrl, {
+      request: {
+        headers: proxyHeaders,
+      },
+    });
+  }
+
+  return null;
+}
+
 export default clerkMiddleware(async (auth, req) => {
+  const proxyResponse = proxyMiddleware(req);
+  if (proxyResponse) {
+    return proxyResponse;
+  }
+
   const { isAuthenticated, sessionClaims } = await auth();
   if (isAuthenticated) {
     await prisma.profile.upsert({
